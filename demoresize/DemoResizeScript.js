23cdb66e10a86512d05bbca4390df616620d7608
Jon <22411356+jjhna@users.noreply.github.com>
2020-04-16 11:26:44 -1000

    Removed client resize
    Removed client resize for demo and included description for future users

--- demoresize/DemoResizeScript.js
+++ demoresize/DemoResizeScript.js
@@ -1,23 +1,5 @@
 "use strict";
 
-function FclientX() {
-    var x = document.getElementById('clientX').value;
-    x = parseInt(x);
-    return(x);
-}
-
-function FclientY() {
-    var y = document.getElementById('clientY').value;
-    y = parseInt(y);
-    return(y);
-}
-
-function FclientZ() {
-    var z = document.getElementById('clientZ').value;
-    z = parseInt(z);
-    return(z);
-}
-
 function FwallX() {
     var x = document.getElementById('wallX').value;
     x = parseInt(x);
