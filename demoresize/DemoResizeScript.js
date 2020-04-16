136dc01a9a45ce25575e4281ac954f779f8dc1db
Jon <22411356+jjhna@users.noreply.github.com>
2020-04-15 15:16:29 -1000

    added demo scripts
    added scripts for demo

---
+++ demoresize/DemoResizeScript.js
@@ -0,0 +1,37 @@
+"use strict";
+
+function FclientX() {
+    var x = document.getElementById('clientX').value;
+    x = parseInt(x);
+    return(x);
+}
+
+function FclientY() {
+    var y = document.getElementById('clientY').value;
+    y = parseInt(y);
+    return(y);
+}
+
+function FclientZ() {
+    var z = document.getElementById('clientZ').value;
+    z = parseInt(z);
+    return(z);
+}
+
+function FwallX() {
+    var x = document.getElementById('wallX').value;
+    x = parseInt(x);
+    return(x);
+}
+
+function FwallY() {
+    var y = document.getElementById('wallY').value;
+    y = parseInt(y);
+    return(y);
+}
+
+function FwallZ() {
+    var z = document.getElementById('wallZ').value;
+    z = parseInt(z);
+    return(z);
+}
